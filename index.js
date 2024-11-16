9bc0cecbc26ab5c2d5ac284bf01a0ed3d2d8cb70
Kristina <kv6543778@gmail.com>
2024-11-17 02:37:09 +0500

    add

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-let post = '';
+let posts = [];
 
-const titleInputNode = document.querySelector('.js-title-input');
+const postTitleInputNode = document.querySelector('.js-post-title-input');
+const postTextInputNode = document.querySelector('.js-post-text-input');
 const newPostBtnNode = document.querySelector('.js-new-post-btn');
 const postsNode = document.querySelector('.js-posts');
 
@@ -8,21 +9,46 @@ const postsNode = document.querySelector('.js-posts');
 newPostBtnNode.addEventListener('click', function() {
     const postFromUser = getPostFromUser();
 
-    setPost(postFromUser);
+    addPost(postFromUser);
 
-    renderPost();
+    renderPosts();
 });
 
 function getPostFromUser() {
-    const post = titleInputNode.value;
+    const title = postTitleInputNode.value;
+    const text = postTextInputNode.value;
 
-    return post;
+    return {
+        title: title,
+        text: text
+    };
 }
 
-function setPost(newPost) {
-    post = newPost;
+function addPost({title, text}) {
+    posts.push({
+        title: title,
+        text: text
+    });
 }
 
-function renderPost(){
-    postsNode.innerText = post;  
+function getPosts() {
+    return posts;
+}
+
+function renderPosts() {
+    const posts = getPosts();
+
+    let postsHTML = '';
+
+    posts.forEach(post=> {
+        postsHTML += `
+        <div class='post'>
+            <p class='post__title'> ${post.title}</p>
+            <p class='post__text'> ${post.text}</p>
+        </div>
+    `;
+
+    }); 
+    
+    postsNode.innerHTML = postsHTML;
 }
